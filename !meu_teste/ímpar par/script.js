aec373c853d02c3fb0f247537259fac3c62a8210
Viictorclay <victorcllay.pe@gmail.com>
2025-08-11 19:05:34 -0300

    11/08/2025

---
+++ !meu_teste/ímpar par/script.js
@@ -0,0 +1,10 @@
+let n1 = 9;
+if (n1 % 2 == 0){
+    console.log(`Número: ${n1}\nSeu número é PAR!`)
+    //window.alert('Seu número é PAR!');
+}
+else{
+    console.log(`Número: ${n1}\nSeu número é ÌMPAR!`)
+    //window.alert('Seu numero é ÌMPAR!');
+}
+    
